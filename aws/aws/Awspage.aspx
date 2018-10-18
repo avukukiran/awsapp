a23baf6c7a2420b66fee168a0d7ad9d75c0a14ad
Kiran <avukukiran1@gmail.com>
2018-10-18 13:40:39 +0530

    2update

---
+++ aws/aws/Awspage.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Awspage.aspx.cs" Inherits="aws.Awspage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+   <table>  
+    <tr>  
+        <td colspan="2">  
+            <h1>Empployee Details</h1>  
+        </td>  
+    </tr>  
+    <tr>  
+        <td>Employee ID</td>  
+        <td>:</td>  
+        <td>  
+            <asp:TextBox ID="txtEmpID" runat="server"></asp:TextBox>  
+        </td>  
+    </tr>  
+    <tr>  
+        <td>Employee Name</td>  
+        <td>:</td>  
+        <td>  
+            <asp:TextBox ID="txtEmpName" runat="server"></asp:TextBox>  
+        </td>  
+    </tr>  
+       <tr>  
+        <td>Gender</td>  
+        <td>:</td>  
+        <td>  
+            <asp:TextBox ID="txtEmpGender" runat="server"></asp:TextBox>  
+        </td>  
+    </tr>
+   <tr>  
+        <td>Mobile</td>  
+        <td>:</td>  
+        <td>  
+            <asp:TextBox ID="txtEmpMobile" runat="server"></asp:TextBox>  
+        </td>  
+    </tr>  
+    <tr>  
+        <td>Employee Email-ID</td>  
+        <td>:</td>  
+        <td>  
+            <asp:TextBox ID="txtEmpEmail" runat="server"></asp:TextBox>  
+        </td>  
+    </tr>  
+    <tr>  
+        <td colspan="2">  
+            <asp:Button ID="btnInsertion" runat="server" Text="Insert" OnClick="btnInsertion_Click" Style="width: 48px" />  
+            <asp:Label ID="lblmsg" runat="server" ForeColor="Red"></asp:Label>  
+        </td>  
+    </tr>  
+</table>  
+</asp:Content>
---
+++ aws/aws/Awspage.aspx.cs
@@ -0,0 +1,51 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace aws
+{
+    public partial class Awspage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnInsertion_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+                /* Insertion After Validations*/
+                using (NpgsqlConnection connection = new NpgsqlConnection())
+                {
+                    connection.ConnectionString = ConfigurationManager.ConnectionStrings["constr"].ToString();
+                    connection.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "Insert into myschema.employee values(@ID,@Name,@Gender,@Email,@Mobile)";
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add(new NpgsqlParameter("@ID", Convert.ToInt32(txtEmpID.Text)));
+                    cmd.Parameters.Add(new NpgsqlParameter("@Name", txtEmpName.Text));
+                    cmd.Parameters.Add(new NpgsqlParameter("@Gender", txtEmpGender.Text));
+                    cmd.Parameters.Add(new NpgsqlParameter("@Email", txtEmpEmail.Text));
+                    cmd.Parameters.Add(new NpgsqlParameter("@Mobile", Convert.ToInt32(txtEmpMobile.Text)));
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                    connection.Close();
+                    txtEmpEmail.Text = "";
+                    txtEmpName.Text = "";
+                    txtEmpID.Text = "";
+                    txtEmpGender.Text = "";
+                    txtEmpMobile.Text = "";
+                    lblmsg.Text = "Data Has been Saved";
+                }
+            //}
+            //catch (Exception ex) { }
+        }
+    }
+}
